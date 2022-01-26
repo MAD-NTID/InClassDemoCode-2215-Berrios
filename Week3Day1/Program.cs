682e1f63a756722f1183ccf498e0a240432c81ac
Michael Berrios <mxbics@rit.edu>
2022-01-26 15:25:07 -0500

    Week3Day1 - Logic (If Statements)

---
+++ Week3Day1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Week3Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Simple Branching Example
+            //string answer_cold = "cold";
+            //string answer_Cold = "Cold";
+
+            //string answer_hot = "hot";
+            //string answer_Hot = "Hot";
+            //string question = "Is hot or cold: ";
+            //string userResponse;
+
+            //Console.Write(question);
+
+            //userResponse = Console.ReadLine();
+
+            ////  for cold
+            //if(answer_cold == userResponse)
+            //{
+            //    Console.WriteLine("1 - Wear a coat, a scarf, boots, and double layer up");
+            //}
+            ////  for Cold
+            //else if(answer_Cold == userResponse)
+            //{
+            //    Console.WriteLine("2 - Wear a coat, a scarf, boots, and double layer up");
+            //}
+            ////  for hot
+            //else if(answer_hot == userResponse)
+            //{
+            //    Console.WriteLine("3 - Get ready to go to Hawaii then, slipper up");
+            //}
+            ////  for Hot
+            //else if(answer_Hot == userResponse)
+            //{
+            //    Console.WriteLine("4 - Get ready to go to Hawaii then, slipper up");
+            //}
+            #endregion
+
+            #region Simple Math
+            //Console.WriteLine("What is your age? ");
+
+            //int userResponse = int.Parse(Console.ReadLine());
+
+            //if (userResponse < 18)
+            //{
+            //    Console.WriteLine("You are not allowed to vote, but have age of consent");
+            //}
+            ////  range comparison using the AND comparison operator
+            ////  any user response between 18 and 20
+            //else if (userResponse >= 18 && userResponse <= 20)
+            //{
+            //    Console.WriteLine("You are allowed to vote, but not allowed to drink");
+            //}
+            //else if (userResponse >= 21)
+            //{
+            //    Console.WriteLine("You are allowed to drink (and many other things)");
+            //}
+            #endregion
+
+            #region Sales Commision
+            //const double COMMISSION_RATE = .07;
+
+            //Console.Write("How much is the sale of the car? ");
+
+            //double sales = double.Parse(Console.ReadLine());
+            //double commission = 0;
+
+            //if(sales >= 10000)
+            //{
+            //    commission = sales * COMMISSION_RATE;
+            //}
+
+            //Console.WriteLine($"Justin earned {commission:C}");
+            #endregion
+        }
+    }
+}
