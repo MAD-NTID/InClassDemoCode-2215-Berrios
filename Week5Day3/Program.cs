ad7fc028cfdd35fcde3eb1b578f487489ce03659
Michael Berrios <mxbics@rit.edu>
2022-02-14 15:09:59 -0500

    Week5Day3

---
+++ Week5Day3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Week5Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region array of ints
+            //int[] numbers = new int[5]; // holds 5 elements
+
+            //Console.WriteLine($"The lenght of the array is {numbers.Length}");
+
+            //numbers[0] = 3;
+            //numbers[1] = 4;
+            //numbers[2] = 27;
+            //numbers[3] = 5;
+            //numbers[4] = 7;
+            ////numbers[5] = 55;    // this crashes the program, it's out of bounds
+
+            //Console.WriteLine($"Index 0: {numbers[0]}");
+            //Console.WriteLine($"Index 1: {numbers[1]}");
+            //Console.WriteLine($"Index 2: {numbers[2]}");
+            //Console.WriteLine($"Index 3: {numbers[3]}");
+            //Console.WriteLine($"Index 4: {numbers[4]}");
+
+            ////  The sum of the array
+            //int sum = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4];
+
+            //Console.WriteLine($"The sum of the array is {sum}");
+
+            //int average = sum / numbers.Length;
+
+            //Console.WriteLine($"The average of the array is {average}");
+            #endregion
+
+            #region array of string
+            //// array to store string objects
+            //string[] strings = new string[3];   // holds 3 elements of string
+
+            //strings[0] = "3";
+            //strings[1] = "4";
+            //strings[2] = "5";
+
+            //int sum = int.Parse(strings[0]) + int.Parse(strings[1]) + int.Parse(strings[2]);
+
+            //Console.WriteLine($"The sum of every element in the string array - \n" +
+            //    $"Index 0: {int.Parse(strings[0])}\n" +
+            //    $"Index 1: {int.Parse(strings[1])}\n" +
+            //    $"Index 2: {int.Parse(strings[2])}\n\n" +
+            //    $"Sum: {sum}");
+            #endregion
+        }
+    }
+}
