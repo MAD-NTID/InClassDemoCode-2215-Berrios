89d1f100c1f9438db6b7f6e3dfc243c9a6336562
Michael Berrios <mxbics@rit.edu>
2022-02-16 15:37:03 -0500

    Resizing Arrays

---
+++ Week6Day1 - Array Resize/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Week6Day1___Array_Resize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is how you resize an array without losing any data
+            //const int INITIAL_SIZE = 5;
+
+            //int[] numbers = new int[INITIAL_SIZE];
+
+            //const int NEW_SIZE = 10;
+
+            //int[] numbersNew = new int[numbers.Length + NEW_SIZE];
+
+            ////  copy the smaller array to the bigger array
+            //numbers.CopyTo(numbersNew, 0);
+
+            ////  overwrite the old array with the new size
+            //numbers = new int[numbers.Length + NEW_SIZE];
+
+            ////  copy the bigger array back to the original array
+            //numbersNew.CopyTo(numbers, 0);
+
+            //  This is how you resize an array but you will lose any data
+            const int INITIAL_SIZE = 5;
+
+            int[] numbers = new int[INITIAL_SIZE];
+
+            const int NEW_SIZE = 10;
+
+            numbers = new int[numbers.Length + NEW_SIZE];
+        }
+    }
+}
