2bbd22eebb3d1f6ad1be3eedbbedebb089b6acb4
Michael Berrios <mxbics@rit.edu>
2022-02-21 14:08:07 -0500

    Auto Initialize Array with Values

---
+++ Week6Day2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Week6Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  The normal traditional
+            string[] traditional = new string[12];
+            traditional[0] = "January";
+            traditional[1] = "February";
+            traditional[2] = "March";
+            traditional[3] = "April";
+            traditional[4] = "May";
+            traditional[5] = "June";
+            traditional[6] = "July";
+            traditional[7] = "August";
+            traditional[8] = "September";
+            traditional[9] = "October";
+            traditional[10] = "November";
+            traditional[11] = "December";
+
+
+            //  How to Initialize an Array with VALUES
+            string[] shortcut = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        }
+    }
+}
