44fdd7900178c992d5778ad18ddb8644bec57fc0
Michael Berrios <mxbics@rit.edu>
2022-02-21 16:20:10 -0500

    Week6Day3

---
+++ Week6Day3 - DoWhile And While Loops/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Week6Day3___DoWhile_And_While_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Number Counter - Ask the user for a start and a end number
+            //  Iterate through the numbers until the test condition fails
+
+            Console.Write("Please enter a number to start counting: ");
+            int startNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter a number to stop counting: ");
+            int stopNumber = int.Parse(Console.ReadLine());
+
+            //  Two Variables to have the same start number on both loops
+            int whileStartNumber = startNumber;
+            int doWhileStartNumber = startNumber;
+
+            #region While Loop
+            //Console.WriteLine("While Loop");
+            //while (whileStartNumber <= stopNumber)
+            //{
+            //    Console.WriteLine($"{whileStartNumber}");
+
+            //    whileStartNumber++;
+            //}
+
+            //Console.WriteLine("-----------------");
+            //Console.WriteLine("Do While");
+            //do
+            //{
+            //    Console.WriteLine($"{doWhileStartNumber}");
+
+            //    doWhileStartNumber++;
+            //} while (doWhileStartNumber <= stopNumber);
+
+            //Console.WriteLine("Finished Counting");
+            #endregion
+
+            #region The Never Ending Loop
+            //while(true)
+            //{
+            //    Console.WriteLine($"{whileStartNumber}");
+            //    whileStartNumber++;
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine($"{doWhileStartNumber}");
+            //    doWhileStartNumber++;
+            //} while (true);
+            #endregion
+        }
+    }
+}
---
+++ Week6Day3 - Validating Input with Loops/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Week6Day3___Validating_Input_with_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region The One Question Jeopardy Game
+            //const int ANSWER = 1965;
+
+            //int year;
+
+            //do
+            //{
+            //    Console.Write($"When was NTID established? ");
+            //    if(int.TryParse(Console.ReadLine(), out year))
+            //    {
+            //        if (year == ANSWER)
+            //        {
+            //            Console.WriteLine("Yay, you win!");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Loser, Try Again!");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Error, please put a number only");
+            //    }
+            //} while (year != ANSWER);
+            #endregion
+
+            #region Username and Password Validations
+            const string USERNAME = "NMAD180";
+            const string PASSWORD = "EAVAFKM";
+
+            while (true)
+            {
+                Console.Write("Please enter your username: ");
+                string username = Console.ReadLine();
+
+                Console.Write("Please enter your password: ");
+                string password = Console.ReadLine();
+
+                if (username == USERNAME && password == PASSWORD)
+                {
+                    Console.WriteLine("Login Correct!");
+
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Login Incorrect!\n");
+                }
+            }
+
+            // THE Student Version
+            //Console.Write("Please enter your username: ");
+            //string username = Console.ReadLine();
+
+            //Console.Write("Please enter your password: ");
+            //string password = Console.ReadLine();
+
+            //if(username == USERNAME && password == PASSWORD)
+            //{
+            //    do
+            //    {
+            //        Console.WriteLine("Login Correct!");
+
+            //        break;
+            //    } while (username == USERNAME && password == PASSWORD);
+            //}
+
+            //else
+            //{
+            //    do
+            //    {
+            //        Console.WriteLine("Login Incorrect!");
+
+            //        break;
+            //    } while (username != USERNAME && password != PASSWORD);
+            //}
+            #endregion
+        }
+    }
+}
