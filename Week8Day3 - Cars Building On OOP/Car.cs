a9d980f11af6eb9cb2d2afe47cfb3f3a6832a9c9
Michael Berrios <mxbics@rit.edu>
2022-03-14 15:23:57 -0400

    Week8Day3 Building On OOP

---
+++ Week8Day3 - Cars Building On OOP/Car.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week8Day3___Cars_Building_On_OOP
+{
+    class Car
+    {
+        const int YEAR_FIRST_CAR_MADE = 1886;
+
+        private string manufacturer;
+
+        public string Manufacturer {
+            get { return manufacturer; } 
+            set
+            {
+                manufacturer = value;
+            }
+        }
+
+        private string model;
+        public string Model { 
+            get { return model;  } 
+            set { model = value; } 
+        }
+
+        private int year;
+        public int Year { 
+            get { return year; }
+            set 
+            { 
+                //  First rule validates for negative
+                if(value < 0)
+                {
+                    value = Math.Abs(value);
+                }
+
+                //  Validates year is not less than the first car ever created
+                if(value < YEAR_FIRST_CAR_MADE)
+                {
+                    value = YEAR_FIRST_CAR_MADE;
+                }
+
+                year = value; 
+            } 
+        }
+
+        private string color;
+        public string Color { 
+            get { return color; } 
+            set { color = value; } 
+        }
+
+        private string type;
+        public string Type { get { return type;  } set { type = value; } }
+
+        private bool electric;
+
+        public bool Electric { get { return electric;  } set { electric = value; } }
+
+        private double msrp;
+
+        public double MSRP { get { return msrp; } set { msrp = value; } }
+
+        ////  The default constructor
+        ////  No parameters
+        //public Car()
+        //{
+        //    manufacturer = "N/A";
+        //    model = "N/A";
+        //    color = "N/A";
+        //    type = "N/A";
+        //    electric = false; // meaning gas
+        //    msrp = 0;
+        //}
+
+        //  The Default constructor
+        //  Sets the default values if not provided to the constructor
+        public Car() : this("N/A", "N/A", "N/A", "N/A", false, 0, YEAR_FIRST_CAR_MADE) { }
+
+        //  This is a parametarized Constructor 
+        //  Meaning, it takes in parameters values
+        public Car(string _manufacturer, string _model, string _color, string _type, bool _electric, double _msrp, int _year)
+        {
+            manufacturer = _manufacturer;
+            model = _model;
+            color = _color;
+            type = _type;
+            electric = _electric;
+            msrp = _msrp;
+            Year = _year;
+        }
+
+        public Car(string _manufacturer, string _model, string _color, string _type, bool _electric, double _msrp)
+        {
+            manufacturer = _manufacturer;
+            model = _model;
+            color = _color;
+            type = _type;
+            electric = _electric;
+            msrp = _msrp;
+            year = DateTime.Now.Year;
+        }
+
+        public override string ToString()
+        {
+            string toString = string.Format(
+                $"Car Info \n" +
+                $"Manufacturer: {manufacturer}\n" +
+                $"Model: {model}\n" +
+                $"Year: {year}\n" +
+                $"Color: {color}\n" +
+                $"Type: {type}\n" +
+                $"Electric: {electric}\n" +
+                $"MSRP: {msrp:C}\n" +
+                $"Monthly for 72 Months {MonthlyPaymentSeventyTwoMonths():C}"
+                );
+
+            return toString;
+        }
+
+        public void StarEngine()
+        {
+            Console.WriteLine("Vroom vroom vroom");
+        }
+
+        public void ShutOffEngine()
+        {
+            Console.WriteLine("Dururtoo too too... (silent)");
+        }
+
+        public double MonthlyPaymentSeventyTwoMonths()
+        {
+            return msrp / 72;
+        }
+    }
+}
---
+++ Week8Day3 - Cars Building On OOP/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Week8Day3___Cars_Building_On_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] cars = new Car[10];
+
+            cars[0] = new Car("Honda", "Civic", "Red", "Hatchback", false, 25000);
+
+            Console.WriteLine(cars[0].ToString());
+
+            cars[1] = new Car("Jeep", "Wrangler", "Red", "SUV", false, 45000, 2021);
+
+            Console.WriteLine();
+            Console.WriteLine(cars[1].ToString());
+
+            cars[2] = new Car();
+            cars[2].Manufacturer = "Subaru";
+            cars[2].Model = "Impreza";
+            cars[2].Color = "Blue";
+            cars[2].Type = "Compact";
+            cars[2].Electric = false;
+            cars[2].Year = -2022;
+
+            Console.WriteLine();
+            Console.WriteLine(cars[2].ToString());
+
+            cars[3] = new Car("Honda", "Pilot", "Black", "SUV", false, 30000, -2014);
+
+            Console.WriteLine();
+            Console.WriteLine(cars[3].ToString());
+        }
+    }
+}
