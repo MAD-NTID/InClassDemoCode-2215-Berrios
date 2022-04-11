3a67e8bc0155dbd88ac342946ac704f94c55e93f
Michael Berrios <mxbics@rit.edu>
2022-04-11 15:59:40 -0400

    Wee11Day3 Utilities

---
+++ Week11Day3_Utilities/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Week11Day3_Utilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string variable = "Something is here";
+
+            //  traditional old way of doing it.
+            //if(variable.IsNullOrEmpty())
+            //{
+            //    Console.WriteLine("Yea, it's null or empty");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No, it has something");
+            //}
+
+            //Console.Write("Do you want chocolate ice cream? (y/n): ");
+            //string response = Console.ReadLine().YessesNays();
+
+            //if(response == "y")
+            //{
+            //    Console.WriteLine("Okay, I'll buy you ice cream");
+            //}
+            //else if(response == "n")
+            //{
+            //    Console.WriteLine("Fine, your loss.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sorry, unknown response");
+            //}
+
+            Console.Write("Enter a word in lowercase to auto upper the first letter: ");
+            string word = Console.ReadLine().ToUpperFirst();
+
+            Console.WriteLine($"The new word is {word}");
+        }
+    }
+}
---
+++ Week11Day3_Utilities/Utility.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week11Day3_Utilities
+{
+    public static class Utility
+    {
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        public static string YessesNays(this string value)
+        {
+            var yessesNays = new Dictionary<string, string>
+            {
+                //  "y" all the yesses
+                {"y", "y"},
+                {"yes", "y"},
+                {"yh", "y" },
+                {"yup", "y" },
+                {"okay", "y" },
+                {"sure", "y" },
+                {"positive", "y" },
+                {"yuppers", "y" },
+                {"si", "y" },
+                {"aye", "y" },
+                {"oui", "y" },
+
+                //  "n" all the nays
+                {"n", "n" },
+                {"no", "n" },
+                {"nope", "n" },
+                {"nay", "n" },
+                {"nopey", "n" },
+                {"nah", "n" },
+                {"negative", "n" }
+            };
+
+            if(value != null && yessesNays.TryGetValue(value.ToLower(), out string finalResponse))
+                return finalResponse;
+
+            return null;
+        }
+
+        public static string ToUpperFirst(this string value)
+        {
+            // The long and ugly way
+            value = value.ToLower();
+
+            char[] theValue = value.ToCharArray();
+
+            theValue[0] = theValue[0].ToString().ToUpper().ToCharArray()[0];
+
+            string newValue = "";
+
+            foreach (char c in theValue)
+                newValue += c;
+
+            return newValue;
+
+            //  More Elegant solution
+            //  Replaces the first character with an upper case of its own character
+            //value = value.Remove(0, 1)
+            //    .Insert(0, value[0].ToString().ToUpper());
+
+            //return value;
+        }
+    }
+}
