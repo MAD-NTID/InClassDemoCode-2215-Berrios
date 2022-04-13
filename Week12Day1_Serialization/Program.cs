888c5341995e0e12070c16184abc879bce06d3c8
Michael Berrios <mxbics@rit.edu>
2022-04-13 15:12:34 -0400

    Week12Day1

---
+++ Week12Day1_Serialization/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+
+namespace Week12Day1_Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string FILE_NAME = "students.json";
+
+            List<Student> students = new();
+
+            students.Add(new Student("Michael", "Berrios", "IT", "Bronx, NY"));
+            students.Add(new Student("Menna", "Nicola", "ACT", "Austin, TX"));
+            students.Add(new Student("Anthony", "Caccese", "ACT", "NJ"));
+            students.Add(new Student("Vincent", "Son", "ACT", "TX"));
+            students.Add(new Student("Evan", "Snyder", "ACT", "OH"));
+            students.Add(new Student("Amir", "Elias", "ACT", "NJ"));
+            students.Add(new Student("Edwin", "Inga", "ACT", "NY"));
+
+            string jsonString = JsonConvert.SerializeObject(students);
+
+            using (StreamWriter writer = new(FILE_NAME))
+            {
+                writer.WriteLine(jsonString);
+            }
+
+            Console.WriteLine("Students has been written to file");
+
+            using (StreamReader reader = new(FILE_NAME))
+            {
+                jsonString = reader.ReadToEnd();
+
+                students = JsonConvert.DeserializeObject<List<Student>>(jsonString);
+            }
+
+            // for each Student in the students List assign to local
+            //  variable student
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Week12Day1_Serialization/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Week12Day1_Serialization
+{
+    class Student
+    {
+        [JsonProperty("First Name")]
+        public string Name { get; set; }
+        [JsonProperty("Last Name")]
+        public string LastName { get; set; }
+        [JsonProperty("Academic Program")]
+        public string Program { get; set; }
+        public string Hometown { get; set; }
+
+        public Student(string name, string lastname, string program, string hometown)
+        {
+            Name = name;
+            LastName = lastname;
+            Program = program;
+            Hometown = hometown;
+        }
+
+        public override string ToString()
+        {
+            return $"First Name: {Name}\nLast Name: {LastName}\nProgram: {Program}\nHometown: {Hometown}";
+        }
+    }
+}
